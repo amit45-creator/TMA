f055e5c77fa7cc620cda2ad95ef836873719a852
amit45-creator <akumargkp2021@gmail.com>
2026-08-01 04:47:23 +0530

    update user profile

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "./error.js"
 import jwt from "jsonwebtoken"
 export const verifyToken=(req,res,next) =>{
+    console.log(req.cookies);//debugging
     const token = req.cookies.access_token
 
     if(!token){
